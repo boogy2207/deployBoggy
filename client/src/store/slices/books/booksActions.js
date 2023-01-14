698486d4d991e4926c2f8d2c5c9e57a1bfbf11f5
johaortiz <surf234@outlook.es>
2023-01-14 17:04:12 -0300

    restructuring

--- client/src/store/slices/books/booksActions.js
+++ client/src/store/slices/books/booksActions.js
@@ -1,15 +1,16 @@
-import axios from 'axios'
-import { getAllBooks, getBooksByTitle } from './index'
+import axios from "axios";
+import { getAllBooks, getBooksByTitle } from "./index";
+
+const urlBack = "https://deployboggy-production.up.railway.app";
 
 export const getBooks = () => (dispatch) => {
-
-    axios("http://localhost:3002/books")
-    .then(res => dispatch(getAllBooks(res.data)))
-    .catch(e => console.log(e))
-}
+  axios(`${urlBack}/books`)
+    .then((res) => dispatch(getAllBooks(res.data)))
+    .catch((e) => console.log(e));
+};
 
 export const getByTitle = (title) => (dispatch) => {
-    axios(`http://localhost:3002/books/title?title=${title}`)
-       .then((res) => dispatch(getBooksByTitle(res.data)))
-       .catch((e) => console.log(e));
- };
+  axios(`${urlBack}/books/title?title=${title}`)
+    .then((res) => dispatch(getBooksByTitle(res.data)))
+    .catch((e) => console.log(e));
+};
