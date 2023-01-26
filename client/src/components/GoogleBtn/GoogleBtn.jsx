649250fd8bfb02f0f03fcf9bba49fada75871c85
johaortiz <surf234@outlook.es>
2023-01-26 11:18:58 -0300

    google changes

--- client/src/components/GoogleBtn/GoogleBtn.jsx
+++ client/src/components/GoogleBtn/GoogleBtn.jsx
@@ -1,96 +1,59 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import jwtdecode from 'jwt-decode'
 import Cookies from 'universal-cookie'
 import { useDispatch } from 'react-redux'
-import { login } from '../../store/slices/auth/requestsUser';
+import { login, register } from '../../store/slices/auth/requestsUser';
 import axios from 'axios';
 
 /* const urlBack = "https://deployboggy-production.up.railway.app" */
 const urlBack = "http://localhost:3002"
 
 const GoogleBtn = () => {
-    const cookies = new Cookies()
-    const dispatch = useDispatch()
-  
-    function handleCallbackResponse (response) {
-      const userObject = jwtdecode(response.credential)
-        console.log(userObject)
-        /* axios.post(`${urlBack}/user/email/${userObject.email}`)
-        .then(res=>{
-            if(!res.data){
-                axios.post(`${urlBack}/user`)
-                .then(res=>console.log(res))
-            }
-        }) */
-        fetch(`${urlBack}/user/email/` + userObject.email)
-      .then(res => res.json())
+  const cookies = new Cookies()
+  const dispatch = useDispatch()
+
+  function handleCallbackResponse(response) {
+    const userObject = jwtdecode(response.credential)
+    console.log(userObject)
+    /* axios.post(`${urlBack}/user/email/${userObject.email}`)
+    .then(res=>{
+        if(!res.data){
+            axios.post(`${urlBack}/user`)
+            .then(res=>console.log(res))
+        }
+    }) */
+    axios(`${urlBack}/user/email/${userObject.email}`)
       .then(data => {
         console.log(data)
         if (!data) {
-          fetch(`${urlBack}/user`, {
-            method: 'POST',
-            body: JSON.stringify({
-              email: userObject.email,
-              password: 'password',
-              name: userObject.name,
-              image: userObject.image
-            }),
-            headers: {
-              'Content-type': 'application/json'
-            },
-            
-          })
-
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data)
-            })
+          dispatch(register({ email: userObject.email, password: "password", name: userObject.name, image: userObject.image }))
         }
-        fetch(`${urlBack}/user/login`, {
-          method: 'POST',
-          body: JSON.stringify({
-            email: userObject.email,
-            password: 'password'
-          }),
-          headers: {
-            'Content-type': 'application/json'
-          },
-          
-        })
+        dispatch(login({ email: userObject.email, password: "password" }))
+      });
+  }
 
-          .then((res) => res.json())
-          .then((data) => {
-           /* dispatch(login({email: userObject.email, password: "password" })) */
-          console.log(data)
-        })
-      }
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '967382346052-h722e070hlc5u7k77or7kqip9scgeob5.apps.googleusercontent.com',
+      callback: handleCallbackResponse
+    })
 
-      )
-       
-    }
-    
-    useEffect(() => {
-      /* global google */
-      google.accounts.id.initialize({
-        client_id: '967382346052-h722e070hlc5u7k77or7kqip9scgeob5.apps.googleusercontent.com',
-        callback: handleCallbackResponse
-      })
-  
-      google.accounts.id.renderButton(
-        document.getElementById('buttonChrome'),
-        { theme: 'outline', size: 'large' }
-      )
-    }, [])// eslint-disable-line
-  
-    const [err, setError] = useState(false)// eslint-disable-line
-    const [success, setSuccess] = useState(false)// eslint-disable-line
-  
-    return (
-      <div
-        className=''
-        id='buttonChrome'
-      />
+    google.accounts.id.renderButton(
+      document.getElementById('buttonChrome'),
+      { theme: 'outline', size: 'large' }
     )
+  }, [])// eslint-disable-line
+
+  const [err, setError] = useState(false)// eslint-disable-line
+  const [success, setSuccess] = useState(false)// eslint-disable-line
+
+  return (
+    <div
+      className=''
+      id='buttonChrome'
+    />
+  )
 }
 
 export default GoogleBtn
